5a532c58db1dadfcec66451e562ae9e2ed28876b
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-12-11 23:59:27 +0330

    add little seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,8 +4,101 @@ import { PrismaClient } from '@prisma/client';
 
 // initialize Prisma Client
 const prisma = new PrismaClient();
-
-async function main() {}
+function groupCodes(): string {
+  const digits: string = '0123456789';
+  let code: string = '';
+  for (let i = 0; i < 5; i++) {
+    code += digits[Math.floor(Math.random() * digits.length)];
+  }
+  return code;
+}
+async function main() {
+  // create client Packages
+  await prisma.packages.createMany({
+    data: [
+      {
+        name: 'golden',
+        price: '1000$',
+        expirationTime: '1month',
+      },
+      {
+        name: 'bronze',
+        price: '800$',
+        expirationTime: '1month',
+      },
+      {
+        name: 'thirds',
+        price: '500$',
+        expirationTime: '1month',
+      },
+    ],
+  });
+  // create clients
+  await prisma.clients.createMany({
+    data: [
+      {
+        company_name: 'josh',
+        company_logo: 'http://goole.com',
+        province: 'tehran',
+        city: 'karaj',
+        Address: '',
+        Phones: '093315432',
+        Socials: ['www.google.com', 'www.gmail.com', 'www.instgram.com'],
+        website: 'www.google.com',
+        cto_name: 'ali',
+        cto_nationCode: '20923123',
+        cto_birthday: '12/12/1400',
+        cto_fatherName: 'reza',
+        cto_phone: '09331231243',
+        email: 'alirmprewe@gmail.com',
+        username: 'alireza',
+        password: 'manirmp1386',
+        packagesId: 2,
+      },
+      {
+        company_name: 'candara',
+        company_logo: 'http://goole.com',
+        province: 'usa',
+        city: 'montreal',
+        Address: '',
+        Phones: '32132',
+        Socials: ['www.google.com', 'www.gmail.com', 'www.instgram.com'],
+        website: 'www.google.com',
+        cto_name: 'seyed',
+        cto_nationCode: '209231233',
+        cto_birthday: '5/2/1343',
+        cto_fatherName: 'mmd',
+        cto_phone: '09331231243',
+        email: 'al342ei@gmail.com',
+        username: 'alireza',
+        password: 'manirmp1386',
+        packagesId: 1,
+      },
+    ],
+  });
+  await prisma.groups.createMany({
+    data: [
+      {
+        name: 'ALIBABA',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+        status: 'ACTIVE',
+        clientId: 12,
+        image: '',
+        groupCodes: groupCodes(),
+      },
+      {
+        name: 'ALIBABA',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+        status: 'ACTIVE',
+        clientId: 11,
+        image: '',
+        groupCodes: groupCodes(),
+      },
+    ],
+  });
+}
 
 // execute the main function
 main()
--- src/utils/RandomCode.ts
+++ src/utils/RandomCode.ts
@@ -1,6 +1,6 @@
-export const codeGenerator = (n) => {
-  const digits = '0123456789';
-  let otp = '';
+export const codeGenerator = (n: number) => {
+  const digits: string = '0123456789';
+  let otp: string = '';
 
   for (let i = 0; i < n; i++) {
     otp += digits[Math.floor(Math.random() * digits.length)];
