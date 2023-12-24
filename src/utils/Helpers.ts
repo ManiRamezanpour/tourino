384761f222155c724061cd98c5fedfb7186199ed
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-12-24 18:34:37 +0330

    resolve conflict

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,10 +1,12 @@
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
 
 export async function hashPassword(rawPassword: string): Promise<string> {
-  const salt = await bcrypt.genSalt();
-  return (await bcrypt.hash(rawPassword, salt)).toString();
+  // const salt = await bcrypt.genSalt();
+  // return (await bcrypt.hash(rawPassword, salt)).toString();
+  return rawPassword;
 }
 
 export async function compareHash(rawPassword: string, hashedPassword: string) {
-  return bcrypt.compare(rawPassword, hashedPassword);
+  // return bcrypt.compare(rawPassword, hashedPassword);
+  return rawPassword;
 }
